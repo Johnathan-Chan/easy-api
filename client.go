a39f37e8bb8cd5d615d3162639a75818ba1002d3
zhesha <zhesha@qingmutec.com>
2022-04-22 14:34:12 +0800

    20220415 fix the handler

--- client.go
+++ client.go
@@ -46,16 +46,12 @@ type IClient interface {
 
 type Middleware interface {
 	UseRequest(...RequestHandler)
-	NextRequestHandler(*http.Request)
 	UseResponse(...ResponseHandler)
-	NextResponseHandler(*http.Response)
 }
 
 type Client struct {
 	httpClient      *http.Client
 	annotationCache map[string]*request
-	beforeIndex     int
-	afterIndex      int
 	beforeRequest   []RequestHandler
 	afterResponse   []ResponseHandler
 	IParse
@@ -75,99 +71,20 @@ func (c *Client) UseRequest(handler ...RequestHandler) {
 	c.beforeRequest = append(c.beforeRequest, handler...)
 }
 
-func (c *Client) NextRequestHandler(req *http.Request) {
-	c.beforeIndex++
-	for c.beforeIndex < len(c.beforeRequest) {
-		c.beforeRequest[c.beforeIndex](req)
-		c.beforeIndex++
-	}
-}
-
 func (c *Client) UseResponse(handler ...ResponseHandler) {
 	c.afterResponse = append(c.afterResponse, handler...)
 }
 
-func (c *Client) NextResponseHandler(resp *http.Response) {
-	c.afterIndex++
-	for c.afterIndex < len(c.afterResponse) {
-		c.afterResponse[c.afterIndex](resp)
-		c.afterIndex++
-	}
-}
 
 func (c *Client) Register(method, path string, object interface{}) {
 	objectType := reflect.TypeOf(object)
 	if objectType.Kind() == reflect.Ptr {
 		objectType = objectType.Elem()
 	}
-
 	c.annotationCache[objectType.Name()] = &request{Method: method, Url: path}
 }
 
-//func (c *Client) Register(pkgPath string) error {
-//	fileSet := token.NewFileSet()
-//	goRegex := regexp.MustCompile("(.go)$")
-//	annotationRegex := regexp.MustCompile(ANNOTION_REQUEST)
-//	methodRegex := regexp.MustCompile(METHOD)
-//	urlRegex := regexp.MustCompile(URL)
-//
-//	if err := filepath.Walk(pkgPath, func(path string, info os.FileInfo, err error) error {
-//		if !goRegex.MatchString(info.Name()) {
-//			return nil
-//		}
-//
-//		decls, err := ScanComments(path, fileSet)
-//		if err != nil {
-//			return err
-//		}
-//
-//		for _, decl := range decls {
-//			genDecl, ok := decl.(*ast.GenDecl)
-//			if !ok || genDecl.Doc == nil {
-//				continue
-//			}
-//
-//			// 解析注解
-//			for _, comment := range genDecl.Doc.List {
-//				if annotationRegex.MatchString(comment.Text) {
-//					annotation := annotationRegex.FindString(comment.Text)
-//					method := methodRegex.FindString(annotation)
-//					method = strings.Replace(method, "\"", "", -1)
-//					method = strings.Replace(method, "method=", "", -1)
-//					_, ok := Method[method]
-//					if !ok {
-//						continue
-//					}
-//
-//					url := urlRegex.FindString(annotation)
-//					url = strings.Replace(url, "\"", "", -1)
-//					url = strings.Replace(url, "url=", "", -1)
-//					if url == "" {
-//						return urlFail
-//					}
-//
-//					for _, v := range genDecl.Specs {
-//						typeSpec, ok := v.(*ast.TypeSpec)
-//						if !ok {
-//							continue
-//						}
-//
-//						c.annotationCache[typeSpec.Name.Obj.Name] = &request{
-//							Method: method,
-//							Url:    url,
-//						}
-//					}
-//				}
-//			}
-//
-//		}
-//		return nil
-//	}); err != nil {
-//		return err
-//	}
-//
-//	return nil
-//}
+
 
 func (c *Client) Do(req, resp interface{}, encode IEncode) error {
 	// 请求解析 req
@@ -186,7 +103,9 @@ func (c *Client) Do(req, resp interface{}, encode IEncode) error {
 	}
 
 	// 执行请求中间件
-	c.NextRequestHandler(httpRequest)
+	for _, beforeRequest := range c.beforeRequest{
+		beforeRequest(httpRequest)
+	}
 	httpResponse, err := c.httpClient.Do(httpRequest)
 	if err != nil {
 		return err
@@ -196,7 +115,9 @@ func (c *Client) Do(req, resp interface{}, encode IEncode) error {
 	}
 
 	// 执行响应中间件
-	c.NextResponseHandler(httpResponse)
+	for _, afterResponse := range c.afterResponse{
+		afterResponse(httpResponse)
+	}
 	if err = encode.Encode(httpResponse, resp); err != nil {
 		return err
 	}
--- example/object/object.go
+++ example/object/object.go
@@ -18,27 +18,27 @@ func SetGlobalMiddleware(req *http.Request){
 		req.Header.Set("Content-Type", "application/json")
 	}
 
-	// 添加 access_token
-	req.Header.Set("Authorization", "Bearer "+ "123123123")
-
-	// 添加 User-Agent
-	req.Header.Set("User-Agent", "fastwego/feishu")
+	//// 添加 access_token
+	//req.Header.Set("Authorization", "Bearer "+ "123123123")
+	//
+	//// 添加 User-Agent
+	//req.Header.Set("User-Agent", "fastwego/feishu")
 }
 
 func main() {
 	client := easy_api.NewClient(easy_api.NewPareRequestArgs())
 
 	// 注册注解
-	client.Register(http.MethodGet, "https://open.feishu.cn/open-apis/sheets/v2/spreadsheets/:SpreadsheetToken/metainfo", GetSheetInfoRequest{})
+	client.Register(http.MethodGet, "https://www.baidu.com", GetSheetInfoRequest{})
 
 	// 请求中间件
 	client.UseRequest(SetGlobalMiddleware)
 
-	// 响应解析器
+	//// 响应解析器
 	jsonPares := easy_api.NewJsonEncode()
 
-	result := make(map[string]interface{})
-	if err := client.Do(GetSheetInfoRequest{
+	var result string
+ 	if err := client.Do(GetSheetInfoRequest{
 		SpreadsheetToken: "shtcnylYjqdPEfyLpNXSpJeSjGc",
 	}, &result, jsonPares); err != nil{
 		panic(err)
